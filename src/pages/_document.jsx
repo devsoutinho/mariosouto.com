a6c924188b524afb6525d17eaf04c4655bcdb35e
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-02-23 15:08:29 -0300

    update

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -51,7 +51,7 @@ export default function Document() {
           href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
         />
       </Head>
-      <body className="flex h-full flex-col bg-zinc-50 dark:bg-black">
+      <body className="flex h-full flex-col bg-zinc-50 dark:bg-zinc-900">
         <Main />
         <NextScript />
       </body>
