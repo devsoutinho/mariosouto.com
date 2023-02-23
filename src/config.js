ad08199a3285a3f3168977580487088228fddd4b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-02-22 21:26:08 -0300

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
   "owner": "Mario Souto",
+  "nickname": "DevSoutinho",
   "title": "Lead Software Engineer, Creator, Speaker, e curioso.",
   "description": "Eu sou Mario Souto, um engenheiro de software nascido no Brasil em São Paulo. Sou criador do canal DevSoutinho e apaixonado por ensinar.",
   "minibio": "Dos servidores de Tibia e Ragnarok em 2011 ao mundo do desenvolvimento de software oficialmente em 2015, @devsoutinho trabalha com foco principal no mundo do Front End, arquitetura, animações, acessibilidade e performance.\nEngenheiro de Software no Nubank e dono do Canal DevSoutinho no YouTube, é apaixonado por compartilhar conhecimento e testar coisas novas no mundo tech.",
--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -9,7 +9,7 @@ export async function generateRssFeed() {
   let articles = await getAllArticles()
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   let author = {
-    name: config.owner,
+    name: config.nickname ? `${config.owner} (${config.nickname})` : config.owner,
     email: config.email,
   }
 
