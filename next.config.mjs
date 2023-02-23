269a9aac7810d6ba9199090c4481d38212132ec7
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-02-23 10:03:10 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 import rehypePrism from '@mapbox/rehype-prism'
+import { redirects } from './redirects.mjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
   },
+  redirects: () => redirects,
   // add image provider on nextjs
   images: {
     domains: ['github.com'],
---
+++ redirects.mjs
@@ -0,0 +1,12 @@
+export const redirects = [
+  {
+    "source": "/lojinha",
+    "destination": "/setup/",
+    "permanent": true
+  },
+  {
+    "source": "/newsletter",
+    "destination": "https://labs.mariosouto.com/newsletter",
+    "permanent": false
+  }
+]
