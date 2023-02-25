4d6963926a4f84e6cccf3299edb1ed8a050ab8ed
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-02-25 14:24:46 -0300

    update

--- src/infra/TagManager/TagManager.jsx
+++ src/infra/TagManager/TagManager.jsx
@@ -4,6 +4,7 @@ const GA_ID = "G-6WPRBC2XSW";
 export function TagManager() {
   return (
     <>
+      {/* Google Tag Manager */}
       <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
@@ -16,6 +17,23 @@ export function TagManager() {
     
       gtag('config', '${GA_ID}');
   `}</Script>
+
+      {/* Meta Pixel */}
+      <Script id="meta-pixel">{`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1383755055752725');
+      fbq('track', 'PageView');
+  `}</Script>
+      <noscript><img height="1" width="1" style={{ display: "none" }}
+        src="https://www.facebook.com/tr?id=1383755055752725&ev=PageView&noscript=1"
+      /></noscript>
     </>
   );
 }
