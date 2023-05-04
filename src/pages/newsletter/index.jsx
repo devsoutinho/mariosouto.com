89f536c9031bd6c3fd2ff83a13eae2557b8a87a9
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-05-04 07:59:27 -0300

    update

--- src/pages/newsletter/index.jsx
+++ src/pages/newsletter/index.jsx
@@ -40,7 +40,7 @@ export default function NewsletterSuccess() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            className="input min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-primary-x500 focus:outline-none focus:ring-4 focus:ring-primary-x500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-primary-x400 dark:focus:ring-primary-x400/10 sm:text-sm"
+            className="input min-w-0 text-zinc-900 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-primary-x500 focus:outline-none focus:ring-4 focus:ring-primary-x500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-primary-x400 dark:focus:ring-primary-x400/10 sm:text-sm"
           />
 
           <button className="g-recaptcha inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-primary-x500 font-semibold text-zinc-100 hover:bg-primary-x400 active:bg-primary-x800 active:text-zinc-100/7 flex-none">
