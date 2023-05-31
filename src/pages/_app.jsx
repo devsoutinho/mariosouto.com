920102a0f3bb00f8559f9d486126fcf2b0b128c0
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-05-30 22:25:25 -0300

    update

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -27,7 +27,7 @@ export default function App({ Component, pageProps, router }) {
       <DarkModeControlProvider>
         <div className="fixed inset-0 flex justify-center sm:px-8">
           <div className="flex w-full max-w-7xl lg:px-8">
-            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+            <div className="w-full bg-white ring-1 ring-zinc-100/10 dark:bg-zinc-900 dark:ring-zinc-300/5" />
           </div>
         </div>
         <div className="relative">
